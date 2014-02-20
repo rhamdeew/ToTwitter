73f8fbf1ac52c3dc756ea07818214f96d18f8a69
Rail Hamdeew <rail@hamdeew.ru>
2014-02-20 23:53:36 +0400

    small confug fix

---
+++ apps/config/config-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rail
+ * Date: 19.02.14
+ * Time: 17:57
+ */
+
+$config = array(
+    "twitter" => array(
+        "consumerKey" => '',
+        "consumerSecret" => '',
+        "accessToken" => '',
+        "accessTokenSecret" => '',
+    ),
+    "bitly" => array(
+        "bitlyKey" => '',
+        "oauthLogin" => '',
+        "oauthToken" => '',
+    )
+);
